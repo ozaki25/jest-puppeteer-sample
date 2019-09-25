6623602395ad960c966cdfd9c08c7f86264b5f76
ozaki <fs65ugumori@yahoo.co.jp>
2019-09-26 07:59:15 +0900

    add jest-puppeteer config

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  server: {
+    command: 'BROWSER=none yarn start',
+    port: 3000,
+  },
+  // use local
+  // launch: {
+  //   dumpio: true,
+  //   headless: false,
+  // },
+};
